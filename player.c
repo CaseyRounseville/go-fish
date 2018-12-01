e689e3c8039ad81a9cfc82fb062b7108bd523fd9
NasserAlhouti <30329703+NasserAlhouti@users.noreply.github.com>
2018-12-01 00:38:44 -0700

    book counter feature implemented
    created a book counter to keep track of who's winning in the game

--- player.c
+++ player.c
@@ -22,8 +22,8 @@ void setUpPlayers(game *g) {
 	printf("How many players this time?: ");
 	int numIntsScanned = scanf("%d", &g->numberOfPlayers);
 	clearInputBuffer();
-
-	while (numIntsScanned == 0 || (g->numberOfPlayers < 2 || g->numberOfPlayers > 7)) {
+// validation detecter if it is not validated it will enter this while statement and it keeps going until the user gets the right range
+	while (numIntsScanned == 0 || (g->numberOfPlayers < 2 || g->numberOfPlayers > 10)) {
 		if (numIntsScanned == 0) {
 			printf("Sorry, I didn't understand that.\n");
 		} else {
@@ -32,13 +32,12 @@ void setUpPlayers(game *g) {
 			} else {
 				printf("There are only 52 cards in the deck.\n");
 			}
-			printf("Please only use integers 2-7.\n");
-			printf("\n");
-
-			printf("How many players this time?: ");
-			numIntsScanned = scanf("%d", &g->numberOfPlayers);
-			clearInputBuffer();
+			printf("Please only use integers 2-10.\n");
+            printf("\n");
 		}
+        printf("How many players this time?: ");
+        numIntsScanned = scanf("%d", &g->numberOfPlayers);
+        clearInputBuffer();
 	}
 
 	// allocate exactly enough memory for the players
@@ -49,6 +48,7 @@ void setUpPlayers(game *g) {
 	char name[100];
 	for (player *p = g->player; p < g->player + g->numberOfPlayers; p++) {
 		char playerPrompt[100];
+        // sprintf goes destination than format string the argument 
 		sprintf(playerPrompt, "Shall player %d be human?(y/n): ", playerNum);
 		if (scanYesOrNo(playerPrompt)) {
 			// the player is human
@@ -113,3 +113,45 @@ int nameIsTaken(player *firstPlayer, int numPlayers, char name[]) {
 	}
 	return 0;
 }
+int bookCounter (player *player){
+      return  deckSize(player->firstCardOfFirstBook)/4;
+}
+// gets player first card then
+// player first
+void moveCardsFromHandToBook(player *player){
+    // looks for each number in the user hand
+    // so in the first interation it would loop throgh the player hand looking for the number of 1's the player has and it keeps doing this up until the 13th type of card
+    for (int value =1 ; value <14; value++) {
+        int count = 0;
+        card *current = player->firstCard;
+        
+        while (current != NULL) {
+            // counts the number of cards in the player hand
+            // ie 7 7 5 5 7 7 5 => (count = 4)
+            if (current->value == value) {
+                count++;
+            }
+            
+            current = current->next;
+        }
+        if (count == 4) {
+            current = player->firstCard;
+            while(current != NULL){
+                card *temp = current->next;
+                if(current->value == value){
+                    player->firstCard = removeCard(player->firstCard,current);
+                    current->next = NULL;
+                    current->prev = NULL;
+                    if (player->firstCardOfFirstBook ) {
+                        appendCard(player->firstCardOfFirstBook, current);
+                    }else{
+                        // is the only card in the list
+                        player->firstCardOfFirstBook = current;
+                    }
+                }
+                current = temp;
+            }
+        }
+    }
+}
+
